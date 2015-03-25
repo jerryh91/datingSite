1784daf347ea628bd2453e25edeed6421b2ccfe9
jerryh91 <jerryhou91@gmail.com>
2015-03-24 21:56:20 -0500

    Test Change

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace ContosoUniversity.Controllers
         public ActionResult Index()
         {
    
-            return View();
+            return View();s
         }
 
         public ActionResult About()
