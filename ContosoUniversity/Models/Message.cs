ef6812a3c83692bf1a82c247f487cbbdcb4fd55d
jerryh91 <jerryhou91@gmail.com>
2015-03-25 20:27:28 -0500

    Initial Working Profile Creation

---
+++ ContosoUniversity/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Message
+    {
+
+        public int ID { get; set; }
+        public int ProfileID {get; set;}
+        public string MessageContent { get; set; }
+
+        //Message : Profile Association
+        //1 Message is sent by only 1 Profile
+        public virtual Profile Profile { get; set; }
+    }
+}
